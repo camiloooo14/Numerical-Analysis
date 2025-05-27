f3381a33cccbf5fdc08e7013c0083820439af14a
Sara Pineda <spinedal@eafit.edu.co>
2025-05-26 21:53:23 -0500

    HELP

--- frontend/src/components/HelpPage.jsx
+++ frontend/src/components/HelpPage.jsx
@@ -58,9 +58,8 @@ export default function HelpPage() {
          <p>Para calcular una derivada en caso de necesitarla, ir a la calculadora de derivadas: https://es.wolframalpha.com/calculators/derivative-calculator</p>   
          <p>Para usar funciones trigonométricas, se debe escribir \sin(x), \cos(x) y \tan(x) respectivamente, no olvidar el backslash.</p>
         </p>
+        <p>Para usar exponentes, de deben escribir entre {}, por ejemplo: x^{2} </p>
       </ul>
-
-
       <footer style={{ marginTop: '2rem', fontSize: '0.9rem', color: '#555' }}>
         Desarrollado por Sara, Camilo, Lorena y Samuel – Proyecto de métodos numéricos
       </footer>
